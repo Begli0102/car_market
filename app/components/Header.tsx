3b0c6eeac49049288092b8cc77aa8dd71c9c347b
Begli Kakabalov <begli.kaka@mail.ru>
2023-11-03 10:39:57 +0100

    update package.json after removing Storybook

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Image from 'next/image'
 import styles from '../page.module.css'
-import { Button } from '@/stories/Button'
 import { Typography, Grid, Chip } from '@mui/material'
 import headerImage from '../../public/hero.png'
 
