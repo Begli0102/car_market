dcb98e481668a0674adcd61811d6848dd4b9ab49
Begli Kakabalov <begli.kaka@mail.ru>
2023-12-27 16:50:26 +0100

    create user model and connect to mongoDB

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,9 +1,15 @@
+import { mongoDB } from '@/app/lib/mongodb'
+import User from '@/app/models/user'
 import { NextResponse } from 'next/server'
+import bcrypt from 'bcryptjs'
 
 export const POST = async (req: Request) => {
   try {
     const body = await req.json()
     const { name, email, password } = body
+    const hashedPassword = bcrypt.hash(password, 10)
+    await mongoDB()
+    await User.create({ name, email, password: hashedPassword })
 
     return NextResponse.json({ message: 'User registered' }, { status: 201 })
   } catch (error) {
