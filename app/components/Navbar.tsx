e87c96dabf366eff5b7d5083d6b337bded4ed42b
Begli Kakabalov <begli.kaka@mail.ru>
2024-01-04 22:46:35 +0100

    Add sign out button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,28 +3,36 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../page.module.css'
 import { Chip } from '@mui/material'
+import { signOut } from 'next-auth/react'
 
-const Navbar = () => (
-  <header className={styles.navbar__container}>
-    <nav className={styles.navbar}>
-      <Link href='/' className={styles.navbar_link}>
-        <Image
-          src='/4ydEzuq5aFVUjXdvHLripG-7eb7ed609239464291ac4b2f1dac2927-autoscout24redesign23-1100.png'
-          alt='logo'
-          width={118}
-          height={40}
-          className={styles.logo_for_navbar}
-          style={{ objectFit: 'contain' }}
-          priority
-        />
-      </Link>
-      <div className={styles.navbar__chip}>
-        <Link href={'/login'}>
-          <Chip label='Sign in' variant='outlined' sx={{ color: '#ffff' }} />
+const Navbar = () => {
+  return (
+    <header className={styles.navbar__container}>
+      <nav className={styles.navbar}>
+        <Link href='/' className={styles.navbar_link}>
+          <Image
+            src='/4ydEzuq5aFVUjXdvHLripG-7eb7ed609239464291ac4b2f1dac2927-autoscout24redesign23-1100.png'
+            alt='logo'
+            width={118}
+            height={40}
+            className={styles.logo_for_navbar}
+            style={{ objectFit: 'contain' }}
+            priority
+          />
         </Link>
-      </div>
-    </nav>
-  </header>
-)
-
+        <div className={styles.navbar__chip}>
+          <Link href={'/login'}>
+            <Chip label='Sign in' variant='outlined' sx={{ color: '#ffff' }} />
+          </Link>
+          <Chip
+            label='Sign out'
+            variant='outlined'
+            sx={{ color: '#ffff' }}
+            onClick={() => signOut()}
+          />
+        </div>
+      </nav>
+    </header>
+  )
+}
 export default Navbar
